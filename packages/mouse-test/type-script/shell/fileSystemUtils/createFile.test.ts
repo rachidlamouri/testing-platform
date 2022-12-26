81ee287702b71ee8abf5a786d13009a54276746e
Rachid Lamouri <rachidlamouri@gmail.com>
2022-12-26 15:50:30 -0500

    Create a mouse shell utility to create files

---
+++ packages/mouse-test/type-script/shell/fileSystemUtils/createFile.test.ts
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import { orchestrate, report, signaler } from "rat-test/type-script/transgressing";
+import { errorUtil } from '../../agnostic/errorUtils';
+import { createFile } from './createFile';
+import { createDirectory, USE_CWD } from './createDirectory';
+
+orchestrate()
+  .then(() => {
+    report('• createMockFile')
+  })
+  .then(() => {
+    report('  ⇀ Testing creating a file')
+
+    const outputFilePath = 'foo/bar';
+    const rootHelper = createDirectory({
+      root: USE_CWD,
+      relativePath: 'foo',
+    });
+    const isOnDiskBefore = fs.existsSync(outputFilePath);
+
+    const creationResult = errorUtil.tryThrowable(() => {
+      return createFile({
+        root: rootHelper,
+        relativePath: 'bar',
+        contents: 'Hello!',
+      });
+    });
+    const isOnDiskAfter = fs.existsSync(outputFilePath)
+    const fileContentsResult = errorUtil.tryThrowable(() => {
+      return fs.readFileSync(outputFilePath, 'utf8');
+    })
+
+    rootHelper.teardown();
+
+    signaler.isTrue(!isOnDiskBefore);
+    signaler.isDeepEqual(creationResult, {
+      didThrow: false,
+      value: {
+        filePath: 'foo/bar',
+        contents: 'Hello!',
+      },
+    })
+    signaler.isTrue(isOnDiskAfter);
+    signaler.isDeepEqual(fileContentsResult, {
+      didThrow: false,
+      value: 'Hello!'
+    });
+  })
