1fafd345669ebc6d38b8f564858da40b3813e318
Rachid Lamouri <rachidlamouri@gmail.com>
2022-12-26 16:27:20 -0500

    Update mouse-test framework test plan

--- packages/mouse-test/scripts/testFramework.sh
+++ packages/mouse-test/scripts/testFramework.sh
@@ -1,2 +1,6 @@
-npm run --silent --workspace mouse-test super:test-type-script "./type-script/errorUtils/tryThrowable.test.ts"
+npm run --silent --workspace mouse-test super:test-type-script\
+  "./type-script/agnostic/errorUtils/tryThrowable.test.ts"\
+  "./type-script/shell/fileSystemUtils/removeFileSystemObject.test.ts"\
+  "./type-script/shell/fileSystemUtils/createDirectory.test.ts"\
+  "./type-script/shell/fileSystemUtils/createFile.test.ts"
 printf "\n"
