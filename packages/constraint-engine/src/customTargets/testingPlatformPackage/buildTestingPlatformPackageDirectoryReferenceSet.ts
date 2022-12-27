f90fb6e2799b0c9d6bed5810bda3280034aee111
Rachid Lamouri <rachidlamouri@gmail.com>
2022-12-27 14:33:49 -0500

    Implement open-schema reference and derived reference builders

---
+++ packages/constraint-engine/src/customTargets/testingPlatformPackage/buildTestingPlatformPackageDirectoryReferenceSet.ts
@@ -0,0 +1,59 @@
+import {
+  TestingPlatformPackageDirectoryTypedTarget,
+  TestingPlatformPackageDirectorySetTypedTarget,
+  TestingPlatformTargetTypeId,
+  TestingPlatformPackageDirectoryTarget,
+} from './targets';
+import { TestingPlatformPackageDirectorySetTargetPath } from './buildTestingPlatformPackageDirectorySetReference';
+import { DerivedReferenceSetBuilder } from '../../types/builder';
+import { PrefixedTargetPath, UnknownTargetPath } from '../../types/targetPath';
+import { TargetReference } from '../../types/targetReference';
+
+export type TestingPlatformPackageDirectoryTargetPath<
+  TPrefix extends UnknownTargetPath,
+> = PrefixedTargetPath<TPrefix, string>;
+
+export type TestingPlatformPackageDirectoryTargetReference<
+  TPrefix extends UnknownTargetPath,
+> = TargetReference<
+  TestingPlatformPackageDirectoryTypedTarget,
+  TestingPlatformPackageDirectoryTargetPath<TPrefix>
+>;
+
+export const buildTestingPlatformPackageDirectoryReferenceSet = (<
+  TPrefix extends UnknownTargetPath,
+>(
+  directorySetReference: TargetReference<
+    TestingPlatformPackageDirectorySetTypedTarget,
+    TPrefix
+  >,
+) => {
+  const targetReferences: TestingPlatformPackageDirectoryTargetReference<TPrefix>[] =
+    directorySetReference.instance.map(
+      (
+        directoryPath,
+        index,
+      ): TestingPlatformPackageDirectoryTargetReference<TPrefix> => {
+        const instance: TestingPlatformPackageDirectoryTarget = {
+          directoryPath,
+          index,
+        };
+
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        const targetPath: TestingPlatformPackageDirectoryTargetPath<TPrefix> = `${directorySetReference.path}/${index}`;
+
+        return {
+          typeId: TestingPlatformTargetTypeId.PackageDirectory,
+          instance,
+          path: targetPath,
+        };
+      },
+    );
+
+  return targetReferences;
+}) satisfies DerivedReferenceSetBuilder<
+  TestingPlatformPackageDirectorySetTypedTarget,
+  TestingPlatformPackageDirectorySetTargetPath,
+  TestingPlatformPackageDirectoryTypedTarget,
+  TestingPlatformPackageDirectoryTargetPath<string>
+>;
--- packages/constraint-engine/src/types/builder.ts
+++ packages/constraint-engine/src/types/builder.ts
@@ -1,6 +1,43 @@
+import { UnknownTypedTarget } from './typedTarget';
+import { UnknownTargetPath } from './targetPath';
+import { TargetReference } from './targetReference';
 import { UnknownTargetInstance } from './targetInstance';
 
 export type InstanceBuilder<
   TInputData extends UnknownTargetInstance,
   TOutputTargetInstance extends UnknownTargetInstance,
 > = (inputData: TInputData) => TOutputTargetInstance;
+
+export type ReferenceBuilder<
+  TInputData extends UnknownTargetInstance,
+  TOutputTypedTarget extends UnknownTypedTarget,
+  TOutputTargetPath extends UnknownTargetPath,
+> = (
+  inputData: TInputData,
+) => TargetReference<TOutputTypedTarget, TOutputTargetPath>;
+
+export type DerivedReferenceBuilder<
+  TInputTypedTarget extends UnknownTypedTarget,
+  TInputTargetPath extends UnknownTargetPath,
+  TOutputTypedTarget extends UnknownTypedTarget,
+  TOutputTargetPath extends UnknownTargetPath,
+> = (
+  inputReference: TargetReference<TInputTypedTarget, TInputTargetPath>,
+) => TargetReference<TOutputTypedTarget, TOutputTargetPath>;
+
+export type ReferenceSetBuilder<
+  TInputData extends UnknownTargetInstance,
+  TOutputTypedTarget extends UnknownTypedTarget,
+  TOutputTargetPath extends UnknownTargetPath,
+> = (
+  inputData: TInputData,
+) => TargetReference<TOutputTypedTarget, TOutputTargetPath>[];
+
+export type DerivedReferenceSetBuilder<
+  TInputTypedTarget extends UnknownTypedTarget,
+  TInputTargetPath extends UnknownTargetPath,
+  TOutputTypedTarget extends UnknownTypedTarget,
+  TOutputTargetPath extends UnknownTargetPath,
+> = (
+  inputReference: TargetReference<TInputTypedTarget, TInputTargetPath>,
+) => TargetReference<TOutputTypedTarget, TOutputTargetPath>[];
