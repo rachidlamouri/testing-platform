b3bfb75e3df299f55aa1d1bebd2ec315a980b05e
Rachid Lamouri <rachidlamouri@gmail.com>
2022-12-30 20:41:50 -0500

    Enforce that all packages have a testing platform configuration object

--- packages/constraint-engine/src/customRules/packageHasPackagefile.ts
+++ packages/constraint-engine/src/customRules/packageHasPackagefile.ts
@@ -5,5 +5,11 @@ export const packageHasPackageFile: Rule<TestingPlatformPackageTarget> = (
   target,
 ) => {
   const { packageFile } = target;
-  return packageFile.isOnDisk && packageFile.isParseable;
+  return (
+    packageFile.isOnDisk &&
+    packageFile.isParseable &&
+    typeof packageFile.parsedContents === 'object' &&
+    packageFile.parsedContents !== null &&
+    !Array.isArray(packageFile.parsedContents)
+  );
 };
--- packages/constraint-engine/src/example/ruleConfigurations.ts
+++ packages/constraint-engine/src/example/ruleConfigurations.ts
@@ -1,5 +1,6 @@
 import { listIsNotEmpty } from '../customRules/listIsNotEmpty';
 import { packageHasPackageFile } from '../customRules/packageHasPackagefile';
+import { packageHasTestingPlatformConfiguration } from '../customRules/packageHasTestingPlatformConfiguration';
 import { packageHasTypeScriptConfigFile } from '../customRules/packageHasTypeScriptConfigFile';
 import { TestingPlatformTargetTypeId } from '../customTargets/testingPlatformPackage/targets';
 import { RuleConfigurationFromTargetReferenceConfigurations } from '../types/ruleConfiguration';
@@ -25,5 +26,10 @@ export const ruleConfigurations: CustomRuleConfiguration[] = [
     targetTypeId: TestingPlatformTargetTypeId.Package,
     normalizedTargetPath: 'testingPlatformPackageDirectorySet/:directoryName',
     rule: packageHasTypeScriptConfigFile,
+  },
+  {
+    targetTypeId: TestingPlatformTargetTypeId.Package,
+    normalizedTargetPath: 'testingPlatformPackageDirectorySet/:directoryName',
+    rule: packageHasTestingPlatformConfiguration,
   },
 ];
