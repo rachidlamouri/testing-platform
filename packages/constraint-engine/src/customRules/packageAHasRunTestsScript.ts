18331d869436a04fbec9853074367c1372b6acfc
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-10 00:22:41 -0500

    Add 'runTestsScript' field to package a type

---
+++ packages/constraint-engine/src/customRules/packageAHasRunTestsScript.ts
@@ -0,0 +1,14 @@
+import { OnDiskUtf8FileTarget } from '../customTargets/file/utf8FileTarget';
+import { TestingPlatformPackageATarget } from '../customTargets/testingPlatformPackage/targets';
+import { GuardRule } from '../types/rule';
+
+type NarrowedPackageATarget = TestingPlatformPackageATarget & {
+  runTestsScript: OnDiskUtf8FileTarget;
+};
+
+export const packageAHasRunTestsScript: GuardRule<
+  TestingPlatformPackageATarget,
+  NarrowedPackageATarget
+> = (target): target is NarrowedPackageATarget => {
+  return target.runTestsScript.isOnDisk;
+};
