63335791735f100bb23110b1a18d8aef5ada2767
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-11 01:23:42 -0500

    Add a base package type

---
+++ packages/constraint-engine/src/customTargets/testingPlatform/basePackage.ts
@@ -0,0 +1,17 @@
+import { JsonFileTarget } from '../file/jsonFile/jsonFileTarget';
+import { Utf8FileTarget } from '../file/utf8File/utf8FileTarget';
+
+type ConfigurablePackageProperties = {
+  packageFile: JsonFileTarget;
+  runTestsScript: Utf8FileTarget;
+  typeScriptConfigFile: JsonFileTarget;
+};
+
+export type BasePackage<
+  TPackageProperties extends ConfigurablePackageProperties,
+> = {
+  directoryName: string;
+  packageFile: TPackageProperties['packageFile'];
+  typeScriptConfigFile: TPackageProperties['typeScriptConfigFile'];
+  runTestsScript: TPackageProperties['runTestsScript'];
+};
