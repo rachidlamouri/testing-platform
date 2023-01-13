1ea910598ec0ad7f7b05072dbacb90641523666b
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-13 00:04:18 -0500

    Populate all runTests scripts

---
+++ packages/mouse-test/scripts/runTests.sh
@@ -0,0 +1,11 @@
+npx ts-node packages/mouse-test/type-script/agnostic/errorUtils/tryThrowable.test.ts
+printf "\n"
+
+npx ts-node packages/mouse-test/type-script/shell/fileSystemUtils/createDirectory.test.ts
+printf "\n"
+
+npx ts-node packages/mouse-test/type-script/shell/fileSystemUtils/createFile.test.ts
+printf "\n"
+
+npx ts-node packages/mouse-test/type-script/shell/fileSystemUtils/removeFileSystemObject.test.ts
+printf "\n"
---
+++ packages/rat-test/scripts/runTests.sh
@@ -0,0 +1,5 @@
+bash packages/rat-test/shell/assertEqual.test.sh
+printf "\n"
+
+bash packages/rat-test/shell/runCommandForEachFile.test.sh
+printf "\n"
