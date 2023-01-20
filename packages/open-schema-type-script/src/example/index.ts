15968ef8ce671ed8fb3a61c17f7485704cb06264
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-20 02:49:13 -0500

    Apply the new datum instance configurations to the example
    Some of the new configurations don't work with the representation
    engine implementation, so we'll have to iterate on that.

--- packages/open-schema-type-script/src/example/index.ts
+++ packages/open-schema-type-script/src/example/index.ts
@@ -3,6 +3,22 @@ import { UnknownBuilderConfigurationTuple } from '../builderConfiguration';
 import { RootDatumInstanceTypeScriptConfiguration } from '../datumInstanceTypeScriptConfiguration';
 import { run } from '../representation-engine/run';
 import {
+  ActualCiYamlFileTypeScriptConfiguration,
+  buildActualCiYamlFileContents,
+} from './testingPlatform/ciYamlFile/actualCiYamlFile';
+import {
+  AssertableCiYamlFileTypeScriptConfiguration,
+  buildAssertableCiYamlFileContentsConfiguration,
+} from './testingPlatform/ciYamlFile/assertableCiYamlFile';
+import {
+  buildExpectedCiYamlContents,
+  ExpectedCiYamlFileContentsTypeScriptConfiguration,
+} from './testingPlatform/ciYamlFile/expectedCiYamlFileContents';
+import {
+  buildExpectedCiYamlFileContentsConfiguration,
+  ExpectedCiYamlFileContentsConfigurationTypeScriptConfiguration,
+} from './testingPlatform/ciYamlFile/expectedCiYamlFileContentsConfiguration';
+import {
   buildPackageDirectoryNameSet,
   PackageDirectoryNameSetTypeScriptConfiguration,
 } from './testingPlatform/packageDirectoryNameSet/packageDirectoryNameSet';
@@ -30,6 +46,47 @@ const builderConfigurationCollection = [
     buildCollection: buildPackageDirectoryNameSet,
     inputCollectionLocatorCollection: [
       'package-directory-name-set-configuration',
+    ],
+  }),
+
+  buildBuilderConfiguration<{
+    InputCollection: [RootDatumInstanceTypeScriptConfiguration];
+    OutputCollection: [ActualCiYamlFileTypeScriptConfiguration];
+  }>({
+    buildCollection: buildActualCiYamlFileContents,
+    inputCollectionLocatorCollection: [''],
+  }),
+  buildBuilderConfiguration<{
+    InputCollection: [RootDatumInstanceTypeScriptConfiguration];
+    OutputCollection: [
+      ExpectedCiYamlFileContentsConfigurationTypeScriptConfiguration,
+    ];
+  }>({
+    buildCollection: buildExpectedCiYamlFileContentsConfiguration,
+    inputCollectionLocatorCollection: [''],
+  }),
+  buildBuilderConfiguration<{
+    InputCollection: [
+      ExpectedCiYamlFileContentsConfigurationTypeScriptConfiguration,
+    ];
+    OutputCollection: [ExpectedCiYamlFileContentsTypeScriptConfiguration];
+  }>({
+    buildCollection: buildExpectedCiYamlContents,
+    inputCollectionLocatorCollection: [
+      'expected-ci-yaml-file-contents-configuration',
+    ],
+  }),
+  buildBuilderConfiguration<{
+    InputCollection: [
+      ActualCiYamlFileTypeScriptConfiguration,
+      ExpectedCiYamlFileContentsTypeScriptConfiguration,
+    ];
+    OutputCollection: [AssertableCiYamlFileTypeScriptConfiguration];
+  }>({
+    buildCollection: buildAssertableCiYamlFileContentsConfiguration,
+    inputCollectionLocatorCollection: [
+      'actual-ci-yaml-file',
+      'expected-ci-yaml-file-contents',
     ],
   }),
 ] as const satisfies UnknownBuilderConfigurationTuple;
