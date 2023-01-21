c2234f20910de0832dc71a9685d57dfdac313e9c
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-21 18:18:42 -0500

    Implement an open-schema validation engine

--- packages/open-schema-type-script/src/datumSemanticsConfiguration.ts
+++ packages/open-schema-type-script/src/datumSemanticsConfiguration.ts
@@ -1,6 +1,19 @@
 import { UnknownCollectionLocator } from './collectionLocator';
+import { UnknownDatumSemanticsProcessor } from './datumSemanticsProcessor';
+import { ConstrainObject } from './utilityTypes/constrainObject';
 
 export type UnknownDatumSemanticsConfiguration = {
   semanticsIdentifier: UnknownCollectionLocator;
   collectionLocator: UnknownCollectionLocator;
+  processDatum: UnknownDatumSemanticsProcessor;
 };
+
+export type DatumSemanticsConfiguration<
+  TUnknownDatumSemanticsConfiguration extends UnknownDatumSemanticsConfiguration,
+> = ConstrainObject<
+  { ConstraintObject: UnknownDatumSemanticsConfiguration },
+  { ConstrainedObject: TUnknownDatumSemanticsConfiguration }
+>;
+
+export type UnknownDatumSemanticsConfigurationTuple =
+  readonly UnknownDatumSemanticsConfiguration[];
