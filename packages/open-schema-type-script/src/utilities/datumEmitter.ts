fefb0082d3358402a969d52229fd6001e46c62a2
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-21 14:40:02 -0500

    Emit datum instance configurations as they are built

---
+++ packages/open-schema-type-script/src/utilities/datumEmitter.ts
@@ -0,0 +1,21 @@
+import EventEmitter from 'events';
+import { UnknownDatumInstance } from '../datumInstance';
+
+const DATUM_EVENT_NAME = 'datum' as const;
+
+export type DatumHandler<TDatum = UnknownDatumInstance> = (
+  datum: TDatum,
+) => void;
+
+// TODO: learn how to use node Streams and re-implement this as a stream
+export class DatumEmitter<TDatum = UnknownDatumInstance> extends EventEmitter {
+  constructor(onDatum: DatumHandler<TDatum>) {
+    super();
+
+    this.on(DATUM_EVENT_NAME, onDatum);
+  }
+
+  emitDatum(datum: TDatum): void {
+    this.emit(DATUM_EVENT_NAME, datum);
+  }
+}
