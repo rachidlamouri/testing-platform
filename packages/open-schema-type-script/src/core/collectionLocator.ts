311096c5ea9b19a2fd7e07428d0521b55d227826
Rachid Lamouri <rachidlamouri@gmail.com>
2023-01-28 13:22:43 -0500

    Separate custom file types from their extension semantics

--- packages/open-schema-type-script/src/core/collectionLocator.ts
+++ packages/open-schema-type-script/src/core/collectionLocator.ts
@@ -2,4 +2,6 @@ import { UnknownString } from '../utilities/types/unknownHelpers';
 
 export type UnknownCollectionLocator = UnknownString;
 
+export type UnknownCollectionLocatorPart = UnknownString;
+
 export type UnknownCollectionLocatorTuple = readonly UnknownCollectionLocator[];
