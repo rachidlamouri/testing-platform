32fefc161c3399c3069c967d5e175d56c65168c8
Rachid Lamouri <rachidlamouri@gmail.com>
2023-02-06 22:10:09 -0500

    Add a custom validation Wortinator

--- packages/open-schema-type-script/src/example/index.ts
+++ packages/open-schema-type-script/src/example/index.ts
@@ -1,12 +1,17 @@
 import { digikikify } from '../core/digikikify';
-import { Estinant } from '../core/estinant';
+import { Estinant, WortinatorEstinant } from '../core/estinant';
 import { Gepp } from '../core/gepp';
 import { Quirm, QuirmTuple } from '../core/quirm';
-import { TropoignantTypeName } from '../core/tropoignant';
+import { TropoignantTypeName, Wortinator } from '../core/tropoignant';
+import { logger } from '../utilities/logger';
 import { blindCastEstinants } from './blindCastEstinants';
 import { fileAEstinant } from './file/fileA';
 import { SIMPLE_FILE_A_CONFIGURATION_QUIRM } from './file/fileAConfiguration';
 import { fileAHasKnownExtensionSuffixEstinant } from './file/fileAHasKnownExtensionSuffix';
+import {
+  ValidationResultOdeshin,
+  VALIDATION_RESULT_GEPP,
+} from './validation/validationResult';
 
 const myGeppA: Gepp = 'example-1';
 const myGeppB: Gepp = 'example-2';
@@ -54,6 +59,21 @@ const myEstinant2: Estinant<string, QuirmTuple<string>> = {
   },
 };
 
+let validValidationCount = 0;
+const errorMessages: string[] = [];
+const myValidationWortinator: Wortinator<ValidationResultOdeshin> = {
+  typeName: TropoignantTypeName.Wortinator,
+  process: function onValidationResult(inputOdeshin) {
+    if (!inputOdeshin.grition.isValid) {
+      errorMessages.push(
+        `"${inputOdeshin.grition.identifier}" does not satisfy "${inputOdeshin.grition.predicate}"`,
+      );
+    } else {
+      validValidationCount += 1;
+    }
+  },
+};
+
 digikikify({
   initialQuirmTuple: [myQuirm1, myQuirm2, SIMPLE_FILE_A_CONFIGURATION_QUIRM],
   estinantTuple: blindCastEstinants([
@@ -61,5 +81,22 @@ digikikify({
     myEstinant2,
     fileAEstinant,
     fileAHasKnownExtensionSuffixEstinant,
+    {
+      inputGepp: VALIDATION_RESULT_GEPP,
+      tropoignant: myValidationWortinator,
+    } satisfies WortinatorEstinant<ValidationResultOdeshin>,
   ]),
 });
+
+logger.feedLine();
+if (errorMessages.length > 0) {
+  errorMessages.forEach((message, index) => {
+    logger.logText(`  Validation Error ${index}`);
+    logger.logText(`  ${message}`);
+    logger.feedLine();
+  });
+
+  process.exit(1);
+} else {
+  logger.logText(`Successful validation count: ${validValidationCount}`);
+}
