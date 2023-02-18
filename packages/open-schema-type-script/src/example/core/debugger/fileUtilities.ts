df00b2021783e98e9a0c8e217be5ab8b89cd0440
Rachid Lamouri <rachidlamouri@gmail.com>
2023-02-17 21:19:52 -0500

    Add identifiable quirm debugger

--- packages/open-schema-type-script/src/example/core/debugger/fileUtilities.ts
+++ packages/open-schema-type-script/src/example/core/debugger/fileUtilities.ts
@@ -5,6 +5,7 @@ import * as uuid from 'uuid';
 const DEBUG_DIR_PATH = './debug/' as const;
 const ENGINE_EVENTS_PATH = posix.join(DEBUG_DIR_PATH, 'engine-events');
 const CACHE_PATH = posix.join(DEBUG_DIR_PATH, 'cache');
+const IDENTITY_CACHE_PATH = posix.join(DEBUG_DIR_PATH, 'identity-cache');
 
 fs.rmSync(DEBUG_DIR_PATH, { recursive: true, force: true });
 fs.mkdirSync(ENGINE_EVENTS_PATH, { recursive: true });
@@ -15,6 +16,12 @@ export const fileUtilities = {
   },
   getCacheFilePath: (directoryName: string): string => {
     return posix.join(CACHE_PATH, directoryName, `${uuid.v4()}.txt`);
+  },
+  getIdentityCacheFilePath: (
+    directoryName: string,
+    identifier: string,
+  ): string => {
+    return posix.join(IDENTITY_CACHE_PATH, directoryName, `${identifier}.txt`);
   },
   writeFile: (filePath: string, text: string): void => {
     fs.mkdirSync(posix.dirname(filePath), { recursive: true });
