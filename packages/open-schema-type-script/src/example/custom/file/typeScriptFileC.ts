b46b826ed1150010b9617d72d77ce9ed0a0ca144
Rachid Lamouri <rachidlamouri@gmail.com>
2023-02-17 23:07:23 -0500

    Update TypeScriptFileC to have import declarations

--- packages/open-schema-type-script/src/example/custom/file/typeScriptFileC.ts
+++ packages/open-schema-type-script/src/example/custom/file/typeScriptFileC.ts
@@ -1,24 +1,31 @@
 import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/typescript-estree';
-import { OnamaEstinant } from '../../../core/estinant';
-import { TropoignantTypeName } from '../../../core/tropoignant';
 import { File } from '../../../utilities/file/file';
 import { FileExtensionSuffixIdentifier } from '../../../utilities/file/fileExtensionSuffixIdentifier';
 import { Grition } from '../../../custom-adapter/grition';
 import { Odeshin } from '../../../custom-adapter/odeshin';
 import { Plifal } from '../../../custom-adapter/plifal';
 import {
-  TypeScriptFileBOdeshin,
+  TypeScriptFileBPlifal,
   TYPE_SCRIPT_FILE_B_GEPP,
 } from './typeScriptFileB';
+import {
+  Ankeler,
+  buildOnamaHamletive,
+} from '../../../type-script-adapter/hamletive/onama';
+import { QuirmOptionTuple } from '../../../type-script-adapter/quirmOptionTuple';
+
+type ImportDeclaration = TSESTree.ImportDeclaration;
+
+type ExportDeclaration =
+  | TSESTree.ExportNamedDeclaration
+  | TSESTree.ExportAllDeclaration;
 
 export type TypeScriptFileC = Grition<
   File<
     FileExtensionSuffixIdentifier.TypeScript,
     {
-      declarations: (
-        | TSESTree.ExportNamedDeclaration
-        | TSESTree.ExportAllDeclaration
-      )[];
+      importDeclarationList: ImportDeclaration[];
+      exportDeclarationList: ExportDeclaration[];
     }
   >
 >;
@@ -41,41 +48,45 @@ export type TypeScriptFileCPlifal = Plifal<
 
 export type TypeScriptFileCPlifalTuple = readonly TypeScriptFileCPlifal[];
 
-export const typeScriptFileCEstinant: OnamaEstinant<
-  TypeScriptFileBOdeshin,
-  TypeScriptFileCPlifalTuple
-> = {
-  inputGepp: TYPE_SCRIPT_FILE_B_GEPP,
-  tropoignant: {
-    typeName: TropoignantTypeName.Onama,
-    process: function createTypeScriptFileC(input) {
-      const output: TypeScriptFileCPlifal = {
-        geppTuple: [TYPE_SCRIPT_FILE_C_GEPP],
-        hubblepup: {
-          identifier: `type-script-file-c:${input.grition.filePath}`,
-          grition: {
-            ...input.grition,
-            additionalMetadata: {
-              declarations:
-                input.grition.additionalMetadata.program.body.filter(
-                  (
-                    statement,
-                  ): statement is
-                    | TSESTree.ExportNamedDeclaration
-                    | TSESTree.ExportAllDeclaration => {
-                    return (
-                      statement.type ===
-                        AST_NODE_TYPES.ExportNamedDeclaration ||
-                      statement.type === AST_NODE_TYPES.ExportAllDeclaration
-                    );
-                  },
-                ),
-            },
-          },
-        },
-      };
+type InputOptionTuple = QuirmOptionTuple<[TypeScriptFileBPlifal]>;
 
-      return [output];
+const buildTypeScriptFileC: Ankeler<InputOptionTuple, TypeScriptFileCPlifal> = (
+  input,
+) => {
+  const { program } = input.hubblepup.grition.additionalMetadata;
+
+  const importDeclarations: ImportDeclaration[] = [];
+  const exportDeclarations: ExportDeclaration[] = [];
+
+  program.body.forEach((statement) => {
+    if (statement.type === AST_NODE_TYPES.ImportDeclaration) {
+      importDeclarations.push(statement);
+    } else if (
+      statement.type === AST_NODE_TYPES.ExportAllDeclaration ||
+      statement.type === AST_NODE_TYPES.ExportNamedDeclaration
+    ) {
+      exportDeclarations.push(statement);
+    }
+  });
+
+  const output: TypeScriptFileCPlifal = {
+    geppTuple: [TYPE_SCRIPT_FILE_C_GEPP],
+    hubblepup: {
+      identifier: `type-script-file-c:${input.hubblepup.grition.filePath}`,
+      grition: {
+        ...input.hubblepup.grition,
+        additionalMetadata: {
+          importDeclarationList: importDeclarations,
+          exportDeclarationList: exportDeclarations,
+        },
+      },
     },
-  },
+  };
+
+  return output;
 };
+
+export const typeScriptFileCEstinant = buildOnamaHamletive({
+  inputGepp: TYPE_SCRIPT_FILE_B_GEPP,
+  ankel: buildTypeScriptFileC,
+});
