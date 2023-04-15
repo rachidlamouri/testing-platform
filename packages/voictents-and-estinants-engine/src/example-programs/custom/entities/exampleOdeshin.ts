022ca8524a7af27448448c99b24ec3730f7cda79
Rachid Lamouri <rachidlamouri@gmail.com>
2023-04-15 14:29:48 -0400

    Remove the example custom program with outdated estinant patterns

--- packages/voictents-and-estinants-engine/src/example-programs/custom/entities/exampleOdeshin.ts
+++
@@ -1,8 +0,0 @@
-import { Grition } from '../../../custom/adapter/grition';
-import { OdeshinFromGrition } from '../../../custom/adapter/odeshin';
-
-export type Example = number;
-
-export type ExampleGrition = Grition<Example>;
-
-export type ExampleOdeshin = OdeshinFromGrition<ExampleGrition>;
