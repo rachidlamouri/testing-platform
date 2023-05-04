c4ddc303163d6ce6a09a73ddb527eb087cb6124b
Rachid Lamouri <rachidlamouri@gmail.com>
2023-05-03 21:58:22 -0400

    Add testBuildAddMetadataForSerialization

--- packages/voictents-and-estinants-engine/ci.sh
+++ packages/voictents-and-estinants-engine/ci.sh
@@ -2,3 +2,6 @@ set -e
 
 npx ts-node packages/voictents-and-estinants-engine/src/example-programs/testJsonSerialization.ts
 bash checkUncommitted.sh
+
+npx ts-node packages/voictents-and-estinants-engine/src/example-programs/testBuildAddMetadataForSerialization.ts
+bash checkUncommitted.sh
