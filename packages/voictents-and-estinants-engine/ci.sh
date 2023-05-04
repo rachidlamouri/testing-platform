f04a179abdd22ed36f7114b11aab50a98601c6aa
Rachid Lamouri <rachidlamouri@gmail.com>
2023-05-03 22:30:54 -0400

    Add more information to ci.sh

--- packages/voictents-and-estinants-engine/ci.sh
+++ packages/voictents-and-estinants-engine/ci.sh
@@ -1,7 +1,11 @@
 set -e
 
+echo "Testing json serialization"
 npx ts-node packages/voictents-and-estinants-engine/src/example-programs/testJsonSerialization.ts
 bash checkUncommitted.sh
+printf "\n"
 
+echo "Testing transforming data for serialization"
 npx ts-node packages/voictents-and-estinants-engine/src/example-programs/testBuildAddMetadataForSerialization.ts
 bash checkUncommitted.sh
+printf "\n"
