5830551ba2221cdc8f77701a3bbabf9b047db370
Rachid Lamouri <rachidlamouri@gmail.com>
2023-05-03 23:25:08 -0400

    Log uncommitted files

--- checkUncommitted.sh
+++ checkUncommitted.sh
@@ -2,6 +2,10 @@ echo "Checking for uncommitted files"
 
 if [[ -n $(git status --porcelain) ]]; then
   echo "Error: Found uncommitted files";
+  echo "Files:"
+
+  git status --porcelain
+
   exit 1;
 fi
 
