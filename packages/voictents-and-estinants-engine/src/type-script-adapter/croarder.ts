49060d68dd8bd7814e49faeba716555f53a7a790
Rachid Lamouri <rachidlamouri@gmail.com>
2023-05-29 16:56:23 -0400

    Remove files marked as unused

--- packages/voictents-and-estinants-engine/src/type-script-adapter/croarder.ts
+++
@@ -1,5 +0,0 @@
-import { Vicken } from './vicken';
-
-export type Croarder<TVicken extends Vicken> = (
-  hubblepup: TVicken['voictentTuple'][number]['hubblepupTuple'][number],
-) => TVicken['zorn'];
