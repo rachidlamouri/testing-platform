e373b7e34627a03f85baa952a8527c271c78987a
Rachid Lamouri <rachidlamouri@gmail.com>
2023-06-04 19:29:33 -0400

    Add a pattern to facilitate reusing getters

---
+++ packages/voictents-and-estinants-engine/src/utilities/buildConstructorFunction.ts
@@ -0,0 +1,118 @@
+import { TypeScriptFunction } from './typed-datum/type-script/function';
+
+/**
+ * Merges two objects without making the intellisense do something dumb, unlike
+ * type-fest. Maybe one day we won't need this
+ */
+type SimpleSimplify<TObject1 extends object, TObject2 extends object> = {
+  [TKey in keyof TObject1 | keyof TObject2]: TKey extends keyof TObject1 &
+    keyof TObject2
+    ? TObject1[TKey] & TObject2[TKey]
+    : TKey extends keyof TObject1
+    ? TObject1[TKey]
+    : TKey extends keyof TObject2
+    ? TObject2[TKey]
+    : never;
+};
+
+export type ObjectWithPrototype<
+  TBaseObject extends object,
+  TPrototype extends object,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = SimpleSimplify<TBaseObject, TPrototype>;
+
+type ConstructorFunction<
+  TBaseObject extends object,
+  TPrototype extends object,
+> = {
+  new (input: TBaseObject): ObjectWithPrototype<TBaseObject, TPrototype>;
+};
+
+type Getter<
+  TBaseObject extends object,
+  TPrototype extends object,
+  TReturnValue,
+> = (self: ObjectWithPrototype<TBaseObject, TPrototype>) => TReturnValue;
+
+type PrototypeConfiguration<
+  TBaseObject extends object,
+  TPrototype extends object,
+> = {
+  [TKey in keyof TPrototype]: Getter<TBaseObject, TPrototype, TPrototype[TKey]>;
+};
+
+type ConstructorFunctionBuilderInput<
+  TConstructorName extends string,
+  TBaseObject extends object,
+  TPrototype extends object,
+> = {
+  constructorName: TConstructorName;
+  prototypeConfiguration: PrototypeConfiguration<TBaseObject, TPrototype>;
+};
+
+type ConstructorFunctionParent<
+  TConstructorName extends string,
+  TBaseObject extends object,
+  TPrototype extends object,
+> = {
+  [Key in TConstructorName]: ConstructorFunction<TBaseObject, TPrototype>;
+};
+
+const buildConstructorFunction = <
+  TConstructorName extends string,
+  TBaseObject extends object,
+  TPrototype extends object,
+>({
+  constructorName,
+  prototypeConfiguration,
+}: ConstructorFunctionBuilderInput<
+  TConstructorName,
+  TBaseObject,
+  TPrototype
+>): ConstructorFunctionParent<TConstructorName, TBaseObject, TPrototype> => {
+  const prototype = {};
+  Object.entries(prototypeConfiguration).forEach(([name, getter]) => {
+    Object.defineProperty(prototype, name, {
+      get() {
+        return (getter as TypeScriptFunction)(this);
+      },
+    });
+  });
+
+  const constructorFunction = Object.assign(
+    // eslint-disable-next-line func-names
+    function (
+      this: ObjectWithPrototype<TBaseObject, TPrototype>,
+      input: TBaseObject,
+    ) {
+      Object.assign(this, input);
+    },
+    {
+      prototype,
+    },
+  ) as unknown as ConstructorFunction<TBaseObject, TPrototype>;
+
+  Object.defineProperty(constructorFunction, 'name', {
+    value: constructorName,
+  });
+
+  const constructorFunctionParent = {
+    [constructorName]: constructorFunction,
+  } as ConstructorFunctionParent<TConstructorName, TBaseObject, TPrototype>;
+
+  return constructorFunctionParent;
+};
+
+export const buildConstructorFunctionWithName = <
+  TConstructorName extends string,
+>(
+  constructorName: TConstructorName,
+) => {
+  return <TBaseObject extends object, TPrototype extends object>(
+    prototypeConfiguration: PrototypeConfiguration<TBaseObject, TPrototype>,
+  ): ConstructorFunctionParent<TConstructorName, TBaseObject, TPrototype> =>
+    buildConstructorFunction<TConstructorName, TBaseObject, TPrototype>({
+      constructorName,
+      prototypeConfiguration,
+    });
+};
