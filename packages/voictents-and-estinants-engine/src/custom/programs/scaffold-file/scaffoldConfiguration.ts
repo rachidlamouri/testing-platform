fdcdaac446b4127d2d73f429668296ebf9c39603
Rachid Lamouri <rachidlamouri@gmail.com>
2023-09-04 16:18:19 -0400

    Update scaffold-file to take the type name first

--- packages/voictents-and-estinants-engine/src/custom/programs/scaffold-file/scaffoldConfiguration.ts
+++ packages/voictents-and-estinants-engine/src/custom/programs/scaffold-file/scaffoldConfiguration.ts
@@ -10,8 +10,8 @@ export const validTypeNameList = Object.values(FileTypeName) as string[];
  * The filePath to create a Voque outline within
  */
 export type ScaffoldConfiguration = {
-  filePath: string;
   typeName: FileTypeName;
+  filePath: string;
 };
 
 export const SCAFFOLD_CONFIGURATION_GEPP = 'scaffold-configuration';
