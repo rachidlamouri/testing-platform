a53c8198767a8ba9f7cae1e827a4b2bda6702312
Rachid Lamouri <rachidlamouri@gmail.com>
2023-09-18 19:46:29 -0400

    Update engine error serialization to show more, but not too much information

--- packages/voictents-and-estinants-engine/src/core/engine/digikikify.ts
+++ packages/voictents-and-estinants-engine/src/core/engine/digikikify.ts
@@ -42,14 +42,40 @@ import { GenericAppreffinge2 } from '../engine-shell/appreffinge/appreffinge2';
 import { Tuple } from '../../utilities/semantic-types/tuple';
 import { getIsRightInputHubblepupTupleAppreffinge } from '../engine-shell/appreffinge/rightInputAppreffinge';
 import { ReferenceTypeName } from '../engine-shell/voictent/referenceTypeName';
+import { assertIsError } from '../../utilities/assertIsError';
 
 class AggregateEngineError extends Error {
-  constructor(errorMessageList: string[]) {
+  constructor(errorList: (string | Error)[]) {
+    const stackTraceList = errorList.map((value) => {
+      if (typeof value === 'string') {
+        return value;
+      }
+
+      return value.stack ?? 'NO STACK TRACE';
+    });
+
     const aggregateMessage = [
-      `Encountered ${errorMessageList.length} errors:`,
-      ...errorMessageList.slice(0, 100).map((errorMessage, index) => {
-        // 4 accounts for 2 spaces and then a 2 digit number
-        return `${`${index}`.padStart(4, ' ')}: ${errorMessage}`;
+      `Encountered ${errorList.length} errors:`,
+      ...stackTraceList.slice(0, 100).map((stackTrace, index) => {
+        const [firstLine, ...otherLineList] = stackTrace.split('\n');
+
+        const truncatedOtherLineList = otherLineList.slice(0, 19);
+
+        const messageSegmentLineList = [
+          // 4 accounts for 2 spaces and then a 2 digit number
+          `${`${index}`.padStart(4, ' ')}: ${firstLine}`,
+          ...truncatedOtherLineList.map((line) => `        ${line}`),
+        ];
+
+        const lineDifference =
+          otherLineList.length - truncatedOtherLineList.length;
+
+        if (lineDifference > 0) {
+          messageSegmentLineList.push(`        +${lineDifference} more lines`);
+        }
+
+        const messageSegment = messageSegmentLineList.join('\n');
+        return messageSegment;
       }),
     ].join('\n');
 
@@ -298,9 +324,11 @@ export const digikikify = ({
     try {
       errorVoictent.addHubblepup(error);
     } catch (secondError) {
+      assertIsError(secondError);
       throw new AggregateEngineError([
         `The engine encountered a critical error. The error voictent "${errorVoictent.gepp}" threw an error while handling an error`,
-        (secondError as Error).message,
+        error.message,
+        secondError.message,
       ]);
     }
 
