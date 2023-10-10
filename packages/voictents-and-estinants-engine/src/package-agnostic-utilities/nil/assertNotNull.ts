a876f784743153d8a60d691640da7de7ec71de40
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-10 17:25:50 -0400

    Apply an automated symbol renaming

--- packages/voictents-and-estinants-engine/src/package-agnostic-utilities/nil/assertNotNull.ts
+++ packages/voictents-and-estinants-engine/src/package-agnostic-utilities/nil/assertNotNull.ts
@@ -1,8 +1,8 @@
 export function assertNotNull<T>(
   datum: T,
-  messsage = 'Expected datum to not be null',
+  message = 'Expected datum to not be null',
 ): asserts datum is Exclude<T, null> {
   if (datum === null) {
-    throw Error(messsage);
+    throw Error(message);
   }
 }
