35b74e854b8c70f943d876e40c953ab4045ac193
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-12 22:44:50 -0400

    Indicate the number of groups that are left to refactor

--- packages/voictents-and-estinants-engine/src/adapted-programs/programs/rename-nonsense/applyRenaming.ts
+++ packages/voictents-and-estinants-engine/src/adapted-programs/programs/rename-nonsense/applyRenaming.ts
@@ -136,6 +136,8 @@ export const applyRenaming = buildProgrammedTransform({
         return;
       }
 
+      log(`Rename groups remaining: ${groupList.length}`);
+
       const {
         directory,
         directoryChange,
