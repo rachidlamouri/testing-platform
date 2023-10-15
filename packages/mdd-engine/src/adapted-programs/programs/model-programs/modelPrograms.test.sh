f5c3850457e81f1a6b174f549f707399c46fdff2
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-15 17:37:58 -0400

    Disable the program modeler in the CI

--- packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.test.sh
+++ packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.test.sh
@@ -1,6 +1,8 @@
-set -e
+# skip
 
-echo "# model-programs"
-echo "Verifying program models have not changed"
-npx ts-node packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.ts
-bash checkUncommittedDebug.sh
+# set -e
+
+# echo "# model-programs"
+# echo "Verifying program models have not changed"
+# npx ts-node packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.ts
+# bash checkUncommittedDebug.sh
