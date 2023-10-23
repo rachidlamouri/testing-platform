7ec43425d529cd9171ff469b8914dab932a92298
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-23 02:07:42 -0400

    Reenable findUnusedExports

--- packages/mdd-engine/src/adapted-programs/programs/find-unused-exports/findUnusedExports.test.sh
+++ packages/mdd-engine/src/adapted-programs/programs/find-unused-exports/findUnusedExports.test.sh
@@ -1,8 +1,6 @@
-# skip
+set -e
 
-# set -e
-
-# echo "# find-unused-exports"
-# echo "Linting unused exports"
-# npx ts-node packages/mdd-engine/src/adapted-programs/programs/find-unused-exports/findUnusedExports.ts
-# bash checkUncommittedDebug.sh
+echo "# find-unused-exports"
+echo "Linting unused exports"
+npx ts-node packages/mdd-engine/src/adapted-programs/programs/find-unused-exports/findUnusedExports.ts
+bash checkUncommittedDebug.sh
