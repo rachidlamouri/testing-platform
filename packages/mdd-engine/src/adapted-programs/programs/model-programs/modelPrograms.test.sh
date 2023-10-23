5b3e7c9d6533b0551f50dd5e10f894fe483423b5
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-22 23:18:08 -0400

    Reenable program modeler test in the CI

--- packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.test.sh
+++ packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.test.sh
@@ -1,8 +1,6 @@
-# skip
+set -e
 
-# set -e
-
-# echo "# model-programs"
-# echo "Verifying program models have not changed"
-# npx ts-node packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.ts
-# bash checkUncommittedDebug.sh
+echo "# model-programs"
+echo "Verifying program models have not changed"
+npx ts-node packages/mdd-engine/src/adapted-programs/programs/model-programs/modelPrograms.ts
+bash checkUncommittedDebug.sh
