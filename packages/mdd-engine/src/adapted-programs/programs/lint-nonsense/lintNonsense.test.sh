cb3391b5d6b768869e5d06fa656cb57653d00220
Rachid Lamouri <rachidlamouri@gmail.com>
2023-10-22 23:21:59 -0400

    Temporarily disable the nonsense linter

--- packages/mdd-engine/src/adapted-programs/programs/lint-nonsense/lintNonsense.test.sh
+++ packages/mdd-engine/src/adapted-programs/programs/lint-nonsense/lintNonsense.test.sh
@@ -1,6 +1,8 @@
-set -e
+# skip
 
-echo "# lint-nonsense"
-echo "Checking for nonsense"
-npx ts-node packages/mdd-engine/src/adapted-programs/programs/lint-nonsense/lintNonsense.ts
-bash checkUncommittedDebug.sh
+# set -e
+
+# echo "# lint-nonsense"
+# echo "Checking for nonsense"
+# npx ts-node packages/mdd-engine/src/adapted-programs/programs/lint-nonsense/lintNonsense.ts
+# bash checkUncommittedDebug.sh
